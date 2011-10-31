6573b97c9f817ce684b2db9e78b3e738c3baeed6
LongTuyen <longtuyen05cntt1@gmail.com>
2011-10-31 16:03:55 +0700

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post
+  include Mongoid::Document
+  field :name, :type => String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppMongoRails::Application.config.secret_token = 'd1952c6a56ea37d7c69ad0ff6009da8f2bbea07c9bd14990e5c1e27c4e556c35544258973284758d74d6eccd76840449b027ed3344b7db35298c2d7cf0128c75'
